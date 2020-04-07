00d37c624b88ca70c0384182e09dd9a5c82eb6de
yoqda <roman.elovskikh@gmail.com>
2020-04-08 00:44:55 +0300

    Homework day2

--- app/server.py
+++ app/server.py
@@ -13,6 +13,14 @@ class ServerProtocol(asyncio.Protocol):
     def __init__(self, server: 'Server'):
         self.server = server
 
+    def is_unic_login(self, login: str):
+        unic = True
+        for client in self.server.clients:
+            if login == client.login:
+                unic = False
+                break
+        return unic
+
     def data_received(self, data: bytes):
         print(data)
 
@@ -22,12 +30,24 @@ class ServerProtocol(asyncio.Protocol):
             self.send_message(decoded)
         else:
             if decoded.startswith("login:"):
-                self.login = decoded.replace("login:", "").replace("\r\n", "")
-                self.transport.write(
-                    f"Привет, {self.login}!\n".encode()
-                )
+                login = decoded.replace("login:", "").replace("\r\n", "")
+                if self.is_unic_login(login):
+                    self.login = login
+                    self.transport.write(
+                        f"Привет, {self.login}!\n".encode()
+                    )
+                    self.send_history()
+                else:
+                    self.transport.write(f"Логин {login} уже занят. Попробуйте другой\n".encode())
+                    self.transport.close()
             else:
                 self.transport.write("Неправильный логин\n".encode())
+
+    def send_history(self):
+        for msg in self.server.last_10_messages:
+            self.transport.write(
+                f"{msg[0]}: {msg[1]}".encode()
+            )
 
     def connection_made(self, transport: transports.Transport):
         self.server.clients.append(self)
@@ -39,7 +59,11 @@ class ServerProtocol(asyncio.Protocol):
         print("Клиент вышел")
 
     def send_message(self, content: str):
-        message = f"{self.login}: {content}\n"
+        message = f"{self.login}: {content}"
+        if len(self.server.last_10_messages) == 10:
+            self.server.last_10_messages = self.server.last_10_messages[1:]
+
+        self.server.last_10_messages.append([self.login, content])
 
         for user in self.server.clients:
             user.transport.write(message.encode())
@@ -47,9 +71,11 @@ class ServerProtocol(asyncio.Protocol):
 
 class Server:
     clients: list
+    last_10_messages: list
 
     def __init__(self):
         self.clients = []
+        self.last_10_messages = []
 
     def build_protocol(self):
         return ServerProtocol(self)
@@ -66,7 +92,6 @@ class Server:
         print("Сервер запущен ...")
 
         await coroutine.serve_forever()
-
 
 process = Server()
 
