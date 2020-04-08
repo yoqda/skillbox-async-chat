e02149421a14a69155b85b440a31ec58ccc8dca4
yoqda <roman.elovskikh@gmail.com>
2020-04-08 21:10:32 +0300

    Homework day2

--- src/day_03/00_handlers.py
+++ src/day_03/00_handlers.py
@@ -1,3 +1,24 @@
 """
 Пример программы для работы с интерфейсами Qt
 """
+from PySide2.QtWidgets import QMainWindow, QApplication
+from src.day_03.interface import Ui_MainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.message_button.clicked.connect(self.button_handler)
+
+    def button_handler(self):
+        self.message_box.appendPlainText(
+            self.message_input.text()
+        )
+        self.message_input.clear()
+
+app = QApplication()
+window = MainWindow()
+window.show()
+app.exec_()
